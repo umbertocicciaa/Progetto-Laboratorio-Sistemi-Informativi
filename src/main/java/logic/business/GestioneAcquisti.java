0c94cf48946896ace7540394815e09cca05bbc0d
BuildTools <unconfigured@null.spigotmc.org>
2024-01-13 19:13:22 +0100

    added CRD Fornitore

--- src/main/java/logic/business/GestioneAcquisti.java
+++ src/main/java/logic/business/GestioneAcquisti.java
@@ -1,4 +1,51 @@
 package logic.business;
 
+import data.Fornitore;
+import logic.HibernateFactory;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author umbertodomenicociccia
+ */
 public interface GestioneAcquisti {
+    SessionFactory sessionFactory = HibernateFactory.ISTANCE.getSessionFactory();
+
+    default void addFornitore(@NotNull String piva, @NotNull String nome, @NotNull String citta) {
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.persist(new Fornitore(piva, nome, citta));
+            session.getTransaction().commit();
+        } catch (HibernateException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    default void removeFornitore(@NotNull String piva) {
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            Fornitore fornitore = session.get(Fornitore.class, piva);
+            session.delete(fornitore);
+            session.getTransaction().commit();
+        } catch (HibernateException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    default Fornitore getFornitore(@NotNull String piva) {
+        Fornitore fornitore = null;
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            fornitore = session.get(Fornitore.class, piva);
+            session.getTransaction().commit();
+            return fornitore;
+        } catch (HibernateException exception) {
+            exception.printStackTrace();
+            return fornitore;
+        }
+
+    }
+
 }
---
+++ src/test/java/logic/business/GestioneAcquistiTest.java
@@ -0,0 +1,67 @@
+package logic.business;
+
+import data.Fornitore;
+import logic.HibernateFactory;
+import org.hibernate.SessionFactory;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author umbertodomenicociccia
+ */
+public class GestioneAcquistiTest {
+    private static SessionFactory sessionFactory;
+    private static GestioneAcquisti gestioneAcquisti;
+
+    @BeforeAll
+    public static void setUp() {
+        sessionFactory = HibernateFactory.ISTANCE.getSessionFactory();
+        gestioneAcquisti = new GestioneAcquisti() {
+        };
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        if (sessionFactory != null && !sessionFactory.isClosed())
+            sessionFactory.close();
+    }
+
+    @Test
+    public void testAddFornitore() {
+        String piva = "F1";
+        String nome = "A";
+        String citta = "Locri";
+
+        gestioneAcquisti.addFornitore(piva, nome, citta);
+        Fornitore fornitore = gestioneAcquisti.getFornitore(piva);
+        Assertions.assertEquals(fornitore, new Fornitore(piva, nome, citta));
+        gestioneAcquisti.removeFornitore(piva);
+    }
+
+    @Test
+    public void testRemoveFornitore() {
+        String piva = "F1";
+        String nome = "A";
+        String citta = "Locri";
+
+        gestioneAcquisti.addFornitore(piva, nome, citta);
+        gestioneAcquisti.removeFornitore(piva);
+        Assertions.assertNull(gestioneAcquisti.getFornitore(piva));
+
+    }
+
+    @Test
+    public void testGetFornitore() {
+        String piva = "F1";
+        String nome = "A";
+        String citta = "Locri";
+
+        gestioneAcquisti.addFornitore(piva, nome, citta);
+        Fornitore fornitore = gestioneAcquisti.getFornitore(piva);
+        Assertions.assertEquals(fornitore, new Fornitore(piva, nome, citta));
+        gestioneAcquisti.removeFornitore(piva);
+    }
+
+}
