5ca17daacd894be745c6d13da65a60510ad18202
BuildTools <unconfigured@null.spigotmc.org>
2024-01-14 19:48:13 +0100

    updated main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,21 @@
-public class Main {
-    public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.util.Objects;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root= FXMLLoader.load(Objects.requireNonNull(getClass().getResource("main.fxml")));
+        stage.setTitle("Consorzio Autolinee Cosenza");
+        stage.setScene(new Scene(root,300,300));
+        stage.show();
     }
 }
