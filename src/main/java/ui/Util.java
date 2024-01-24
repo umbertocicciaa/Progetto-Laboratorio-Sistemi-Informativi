4d9e97f261c50a9d509731895843ed14f8f810f3
BuildTools <unconfigured@null.spigotmc.org>
2024-01-24 18:59:11 +0100

    util class dor fix duplication

---
+++ src/main/java/ui/Util.java
@@ -0,0 +1,22 @@
+package ui;
+/**
+ * @author umbertodomenicociccia
+ * */
+public final class Util {
+    private final static String select = "select";
+    private final static String from = "from";
+    private final static String where = "where";
+
+    public static String getSelect() {
+        return select;
+    }
+
+    public static String getFrom() {
+        return from;
+    }
+
+    public static String getWhere() {
+        return where;
+    }
+
+}
