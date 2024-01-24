58b8266ec1559df057318b523376323ae8d02543
BuildTools <unconfigured@null.spigotmc.org>
2024-01-24 18:59:32 +0100

    fixed add parameter check

--- src/main/java/ui/acquisti/AddFornitoreController.java
+++ src/main/java/ui/acquisti/AddFornitoreController.java
@@ -2,12 +2,15 @@ package ui.acquisti;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 import logic.business.GestioneAcquisti;
 import org.hibernate.HibernateException;
+import ui.Util;
 
+import javax.swing.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 /**
@@ -38,15 +41,27 @@ public class AddFornitoreController implements Initializable {
         String nome = nomeField.getText();
         String citta = cittaField.getText();
 
-        if (!piva.isEmpty() && !nome.isEmpty() && !citta.isEmpty()) {
+        if (isVerifica(piva, nome, citta)) {
             try {
                 gestioneAcquisti.addFornitore(piva, nome, citta);
             } catch (HibernateException ex) {
                 ex.printStackTrace();
             }
+        }else{
+            Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+            errorAlert.setHeaderText("Errore!");
+            errorAlert.setContentText("Hai fornito parametri scorretti");
+            errorAlert.showAndWait();
         }
         Stage stage = (Stage) okButton.getScene().getWindow();
         stage.close();
+    }
+
+    private static boolean isVerifica(String piva, String nome, String citta) {
+        boolean conditionPiva=!piva.isEmpty()&& !piva.toLowerCase().contains(Util.getFrom()) && !piva.toLowerCase().contains(Util.getWhere()) && !piva.toLowerCase().contains(Util.getSelect());
+        boolean conditioNome=!nome.isEmpty() && !nome.toLowerCase().contains(Util.getFrom()) && !nome.toLowerCase().contains(Util.getWhere()) && !nome.toLowerCase().contains(Util.getSelect());
+        boolean conditionCitta=!citta.isEmpty() && !citta.toLowerCase().contains(Util.getFrom()) && !citta.toLowerCase().contains(Util.getWhere()) && !citta.toLowerCase().contains(Util.getSelect());
+        return  conditionPiva && conditioNome && conditionCitta;
     }
 
     public void handleCancelButton() {
