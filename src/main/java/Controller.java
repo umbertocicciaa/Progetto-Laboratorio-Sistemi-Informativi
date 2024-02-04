3802eff8f11329ee2e3c0a6cc535eddd511ddb29
BuildTools <unconfigured@null.spigotmc.org>
2024-02-04 15:08:40 +0100

    refactor

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,18 +1,12 @@
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.input.MouseEvent;
-import javafx.stage.Stage;
 import ui.UIUtil;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.Objects;
 import java.util.ResourceBundle;
 
 /**
@@ -32,40 +26,6 @@ public class Controller implements Initializable {
 
     public void selectItem(MouseEvent event) throws IOException {
         TreeItem<String> item = homeTreeView.getSelectionModel().getSelectedItem();
-        if (item != null) {
-            switch (item.getValue()) {
-                case "Fornitore" -> {
-                    Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/ui/acquisti/fornitori.fxml")));
-                    Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-                    Scene scene = new Scene(root);
-                    stage.setScene(scene);
-                    stage.show();
-                }
-                case "Automezzo Da Ordinare" -> {
-                    Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/ui/automezzo/automezzo.fxml")));
-                    Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-                    Scene scene = new Scene(root);
-                    stage.setScene(scene);
-                    stage.show();
-                }
-                case "Prodotto Da Ordinare" -> {
-                    Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/ui/prodotto/prodotto.fxml")));
-                    Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-                    Scene scene = new Scene(root);
-                    stage.setScene(scene);
-                    stage.show();
-                }
-                case "Ordine" -> {
-                    // Handle Ordine case
-                    Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/ui/ordine/ordine.fxml")));
-                    Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-                    Scene scene = new Scene(root);
-                    stage.setScene(scene);
-                    stage.show();
-                }
-                default -> {
-                }
-            }
-        }
+        UIUtil.loadSelectedItem(item, event);
     }
 }
